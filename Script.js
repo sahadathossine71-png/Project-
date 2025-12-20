e0a268340934669bfe7ae1d299e33dc1c901acd4
sahadathossine71-png <sahadathossine71@gmail.com>
2025-12-20 19:17:36 +0600

    Add mousemove event and GSAP animations

--- Script.js
+++ Script.js
@@ -37,3 +37,50 @@ function a () {
     })
 }
 a()
+
+
+let s= document.querySelector(".main")
+let b = document.querySelector(".cursor")
+
+s.addEventListener("mousemove", function (dets) {
+    gsap.to(b,{
+        x:dets.x,
+        y:dets.y
+        
+    })
+})
+
+
+gsap.from("nav h1",{
+    y:-100,
+    opacity:0,
+    duration:1.5,
+    delay:0.4
+    
+})
+gsap.from(".container > div",{
+    y:-100,
+    opacity:0,
+    duration:0.6,
+    delay:0.8,
+    stagger:0.3
+})
+gsap.from("nav .butt",{
+    opacity:0,
+    scale:0.4,
+    delay:1
+    
+})
+
+gsap.to(".review  .card", {
+    x:-2000,
+    duration:2,
+    delay:2,
+    scrollTrigger:{
+        trigger:".review",
+        pin:true,
+        start:"top top",
+        markers:true,
+        scrub:1
+    }
+})
